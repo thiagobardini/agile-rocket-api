30fca101b7e0992cac58d2c239014e7988028047
thiagobadini <thiagobardini@hotmail.com>
2021-12-26 18:37:24 -0500

    Fixing Cors - Bug

--- server.js
+++ server.js
@@ -51,9 +51,8 @@ app.use(auth)
 // The method `.use` sets up middleware for the Express application
 app.use(express.json())
 
-
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://mysterious-sierra-58663.herokuapp.com/sign-in')
+  res.header('Access-Control-Allow-Origin', 'https://thiagobardini.com/agile-rocket-client/#/')
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
