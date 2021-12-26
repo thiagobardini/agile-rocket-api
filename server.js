f8141d2537e8b1adf586add4b78e1cb795318552
thiagobadini <thiagobardini@hotmail.com>
2021-12-26 17:54:50 -0500

    Fixing Cors - Bug

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(auth)
 app.use(express.json())
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', 'https://thiagobardini.com/agile-rocket-client')
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
