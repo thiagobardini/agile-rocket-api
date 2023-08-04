9266252629d10caebc929b748ef5e0a8eaddd37d
thiagobadini <thiagobardini@hotmail.com>
2023-08-04 15:07:30 -0400

    Fixed the nodemon app name

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'agile-rocket-api'
+const mongooseBaseName = 'agile-rocket'
 
 // create the mongodb uri for development and test
 const database = {
